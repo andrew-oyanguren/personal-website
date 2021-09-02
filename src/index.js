8de52243778c43a82b8a3d325c2f67de34e34025
Andrew Oyanguren <andrewoyanguren@Andrews-MacBook-Pro.local>
2021-09-01 20:09:43 -0700

    Created globalStyles component
    Added global style  presets to project

--- src/index.js
+++ src/index.js
@@ -1,12 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
 import App from './App';
 import { BrowserRouter } from 'react-router-dom';
+import GlobalStyles from './theme/globalStyles';
 
 ReactDOM.render(
   <React.StrictMode>
     <BrowserRouter>
+      <GlobalStyles />
       <App />
     </BrowserRouter>
   </React.StrictMode>,
